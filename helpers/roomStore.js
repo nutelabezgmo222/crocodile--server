ad4737cdaaecca4a0380e0983d373f4b0c2bdf16
Maksym Solodovnykov <63587682+nutelabezgmo222@users.noreply.github.com>
2021-06-10 15:13:42 +0300

    change max room users

--- helpers/roomStore.js
+++ helpers/roomStore.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 const __dirname = path.resolve();
-const maxRoomUsers = 3;
+const maxRoomUsers = 5;
 
 class RoomStore {
   createNewRoom(host, isRoomOpen) {}
